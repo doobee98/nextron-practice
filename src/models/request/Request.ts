236ea273cee4f2062d7b13024a411a8af2238b3b
2doo <2doo@kakao.com>
2021-11-14 13:14:33 +0900

    [FEAT] add authrization in request

--- src/models/request/Request.ts
+++ src/models/request/Request.ts
@@ -1,5 +1,6 @@
 interface Request {
   method: 'GET' | 'POST' | 'DELETE'; // 현재는 rest api 형태가 아니므로 사용하지 않지만 확장을 위해 남겨둠
+  authrization?: string;
 }
 
 export default Request;
--- src/models/response/auth/LoginResponse.ts
+++ src/models/response/auth/LoginResponse.ts
@@ -2,6 +2,7 @@ import Response from '../Response';
 
 interface LoginResponse extends Response {
   isSuccess: boolean;
+  authrization?: string;
 }
 
 export default LoginResponse;
